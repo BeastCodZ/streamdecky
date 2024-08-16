afde3c71b705b5b6a7accc65b1b13eaa362375c1
BeastCodZ <62057520+BeastCodZ@users.noreply.github.com>
2024-08-16 15:04:14 +0530

    removed logging

--- server.js
+++ server.js
@@ -70,12 +70,12 @@ server.post("/api/status", (req, res) => {
   }
 
   const processName = appSetting.processName;
-  console.log(processName);
+  
   exec(`tasklist /fi "ImageName eq ${processName}"`, (err, stdout) => {
     if (err) {
       return res.status(200).json({ message: "Not Running" });
     }
-    console.log(stdout);
+  
     const isRunning = stdout.toLowerCase().includes(processName.toLowerCase().substring(0,25))
     res.status(200).json({ isRunning });
   });
